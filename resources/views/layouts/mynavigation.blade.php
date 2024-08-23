0b226c1d7d1ebd3a5eb226bfc4c46c37428d0999
rsudanta <rsudanta20@gmail.com>
2024-08-23 16:18:18 +0700

    first commit

---
+++ resources/views/layouts/mynavigation.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg sticky-top" style="background: white;">
+    <div class="container">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto flex-nowrap">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">PRODUCT</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">GALLERY</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">BLOG</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">MY INVENTORY</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
